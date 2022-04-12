0043cbbb35ec2bb3dd4924cf140f3e29dfb17129
Samuel Colvin <s@muelcolvin.com>
2022-04-13 00:14:12 +0100

    fix coverage generate

--- tests/prepare_coverage.sh
+++ tests/prepare_coverage.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rust-profdata merge -sparse default.profraw -o default.profdata
-rust-cov export -Xdemangler=rustfilt target/debug/lib_pydantic_core.dylib \
-    -instr-profile=default.profdata \
+rust-profdata merge -sparse rust_coverage.profraw -o rust_coverage.profdata
+rust-cov export -Xdemangler=rustfilt $(ls pydantic_core/*.so) \
+    -instr-profile=rust_coverage.profdata \
     --ignore-filename-regex='/.cargo/registry' \
     --ignore-filename-regex='library/std' \
     -format=lcov > rust_coverage.lcov
