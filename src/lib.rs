4bc00414ae1eb06487d8ee3ce0b9dd054225b8ec
messense <messense@icloud.com>
2022-06-04 16:16:10 +0800

    Build wheels on CI

--- src/lib.rs
+++ src/lib.rs
@@ -24,13 +24,18 @@ mod validators;
 
 create_exception!(_pydantic_core, SchemaError, PyException);
 
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-
 #[pymodule]
 fn _pydantic_core(py: Python, m: &PyModule) -> PyResult<()> {
+    let mut version = env!("CARGO_PKG_VERSION").to_string();
+    // cargo uses "1.0-alpha1" etc. while python uses "1.0.0a1", this is not full compatibility,
+    // but it's good enough for now
+    // see https://docs.rs/semver/1.0.9/semver/struct.Version.html#method.parse for rust spec
+    // see https://peps.python.org/pep-0440/ for python spec
+    // it seems the dot after "alpha/beta" e.g. "-alpha.1" is not necessary, hence why this works
+    version = version.replace("-alpha", "a").replace("-beta", "b");
+    m.add("__version__", version)?;
     m.add("ValidationError", py.get_type::<errors::ValidationError>())?;
     m.add("SchemaError", py.get_type::<SchemaError>())?;
-    m.add("__version__", VERSION)?;
     m.add_class::<validators::SchemaValidator>()?;
     Ok(())
 }
