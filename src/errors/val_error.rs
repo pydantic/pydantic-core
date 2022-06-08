c9ffc6478ef1eb1e8d89b1a543b5ecde504f71a8
Samuel Colvin <s@muelcolvin.com>
2022-06-08 14:34:40 +0100

    improving coverage

--- src/errors/val_error.rs
+++ src/errors/val_error.rs
@@ -1,5 +1,3 @@
-use std::error::Error;
-use std::fmt;
 use std::result::Result as StdResult;
 
 use pyo3::prelude::*;
@@ -12,28 +10,6 @@ pub type ValResult<'a, T> = StdResult<T, ValError<'a>>;
 pub enum ValError<'a> {
     LineErrors(Vec<ValLineError<'a>>),
     InternalErr(PyErr),
-}
-
-impl<'a> fmt::Display for ValError<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ValError::LineErrors(line_errors) => {
-                write!(f, "Line errors: {:?}", line_errors)
-            }
-            ValError::InternalErr(err) => {
-                write!(f, "Internal error: {}", err)
-            }
-        }
-    }
-}
-
-impl<'a> Error for ValError<'a> {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            ValError::LineErrors(_errors) => None,
-            ValError::InternalErr(err) => Some(err),
-        }
-    }
 }
 
 pub fn as_internal<'a>(err: PyErr) -> ValError<'a> {
