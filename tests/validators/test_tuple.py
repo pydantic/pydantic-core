991b877f49ef2d894a0257854dd7b970423d3e88
czotomo <beltowski.t@gmail.com>
2022-06-12 00:55:54 +0200

    add fix length tuple error tests

--- tests/validators/test_tuple.py
+++ tests/validators/test_tuple.py
@@ -28,7 +28,8 @@ def test_tuple_strict_passes_with_tuple(tuple_variant, items, input, expected):
 
 
 @pytest.mark.parametrize(
-    'tuple_variant,items', [('tuple-var-len', {'type': 'int'}), ('tuple-fix-len', [{'type': 'int'}])]
+    'tuple_variant,items',
+    [('tuple-var-len', {'type': 'int'}), ('tuple-fix-len', [{'type': 'int'}, {'type': 'int'}, {'type': 'int'}])],
 )
 @pytest.mark.parametrize('wrong_coll_type', [list, set, frozenset])
 def test_tuple_strict_fails_without_tuple(wrong_coll_type, tuple_variant, items):
@@ -52,7 +53,7 @@ def test_tuple_strict_fails_without_tuple(wrong_coll_type, tuple_variant, items)
         ({'max_items': 3}, (1, 2, 3, 4), Err('Tuple must have at most 3 items [kind=tuple_too_long,')),
     ],
 )
-def test_tuple_kwargs(kwargs, input_value, expected):
+def test_tuple_var_len_kwargs(kwargs, input_value, expected):
     v = SchemaValidator({'type': 'tuple-var-len', **kwargs})
     if isinstance(expected, Err):
         with pytest.raises(ValidationError, match=re.escape(expected.message)):
@@ -73,7 +74,7 @@ def test_tuple_kwargs(kwargs, input_value, expected):
         (frozenset([1, 2, '3']), IsTuple(1, 2, 3, check_order=False)),
     ],
 )
-def test_tuple_var_len(input_value, expected, tuple_variant, items):
+def test_tuple_validate(input_value, expected, tuple_variant, items):
     v = SchemaValidator({'type': tuple_variant, 'items': items})
     assert v.validate_python(input_value) == expected
 
@@ -89,8 +90,37 @@ def test_tuple_var_len(input_value, expected, tuple_variant, items):
         ((1, 2, 'wrong'), IsNonNegative()),
     ],
 )
-def test_tuple_errors(input_value, index):
+def test_tuple_var_len_errors(input_value, index):
     v = SchemaValidator({'type': 'tuple-var-len', 'items': {'type': 'int'}})
+    with pytest.raises(ValidationError) as exc_info:
+        assert v.validate_python(input_value)
+    assert exc_info.value.errors() == [
+        {
+            'kind': 'int_parsing',
+            'loc': [index],
+            'message': 'Value must be a valid integer, unable to parse string as an integer',
+            'input_value': 'wrong',
+        }
+    ]
+
+
+@pytest.mark.parametrize(
+    'input_value,items,index',
+    [
+        (['wrong'], [{'type': 'int'}], 0),
+        (('wrong',), [{'type': 'int'}], 0),
+        ({'wrong'}, [{'type': 'int'}], 0),
+        ((1, 2, 3, 'wrong'), [{'type': 'int'}, {'type': 'int'}, {'type': 'int'}, {'type': 'int'}], 3),
+        (
+            (1, 2, 3, 'wrong', 4),
+            [{'type': 'int'}, {'type': 'int'}, {'type': 'int'}, {'type': 'int'}, {'type': 'int'}],
+            3,
+        ),
+        ((1, 2, 'wrong'), [{'type': 'int'}, {'type': 'int'}, {'type': 'int'}], IsNonNegative()),
+    ],
+)
+def test_tuple_fix_len_errors(input_value, items, index):
+    v = SchemaValidator({'type': 'tuple-fix-len', 'items': items})
     with pytest.raises(ValidationError) as exc_info:
         assert v.validate_python(input_value)
     assert exc_info.value.errors() == [
