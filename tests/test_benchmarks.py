a503a7713d8671d20e66f778bec256786350b80b
dswij <dswijj@gmail.com>
2022-06-14 15:01:33 +0800

    Add benchmark for bytes type

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -429,3 +429,35 @@ def test_list_of_optional_core(benchmark):
     v = SchemaValidator({'type': 'list', 'items': {'type': 'optional', 'schema': 'int'}})
 
     benchmark(v.validate_python, list_of_optional_data)
+
+
+long_bytes = b'0' * 1000
+list_of_long_bytes = [long_bytes * 1000]
+
+@pytest.mark.benchmark(group='bytes')
+def test_long_bytes_core(benchmark):
+    v = SchemaValidator({'type': 'bytes'})
+
+    benchmark(v.validate_python, long_bytes)
+
+@pytest.mark.benchmark(group='bytes')
+def test_list_of_long_bytes_core(benchmark):
+    v = SchemaValidator({'type': 'list', 'items': {'type': 'bytes'}})
+
+    benchmark(v.validate_python, list_of_long_bytes)
+
+@skip_pydantic
+@pytest.mark.benchmark(group='bytes')
+def test_long_bytes_pyd(benchmark):
+    class PydanticModel(BaseModel):
+        __root__: bytes 
+
+    benchmark(PydanticModel.parse_obj, long_bytes)
+
+@skip_pydantic
+@pytest.mark.benchmark(group='bytes')
+def test_list_of_long_bytes_pyd(benchmark):
+    class PydanticModel(BaseModel):
+        __root__: List[bytes]
+
+    benchmark(PydanticModel.parse_obj, list_of_long_bytes)
