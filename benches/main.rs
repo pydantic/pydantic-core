6ec8c2672c13fb9ba7d54359d25ea90868db7eb5
Samuel Colvin <s@muelcolvin.com>
2022-06-15 15:25:08 +0100

    tweaking model dict creation

--- benches/main.rs
+++ benches/main.rs
@@ -1,5 +1,5 @@
-use std::time::Duration;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::time::Duration;
 
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
@@ -48,14 +48,7 @@ pub fn benchmark_dict(c: &mut Criterion) {
                 "b": 2,
                 "c": 3,
                 "d": 4,
-                "e": 5,
-                "f": 6,
-                "g": 7,
-                "h": 8,
-                "i": 9,
-                "j": 10,
-                "k": 11,
-                "l": 12
+                "e": 5
             }
             "#
                 .to_string(),
@@ -65,9 +58,46 @@ pub fn benchmark_dict(c: &mut Criterion) {
     });
 }
 
-criterion_group!{
+pub fn benchmark_model(c: &mut Criterion) {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let validator = build_schema_validator(
+        py,
+        r#"{
+          'type': 'model',
+          'extra': 'ignore',
+          'fields': {
+            'a': 'int',
+            'b': 'int',
+            'c': 'int',
+            'd': 'int',
+            'e': 'int',
+          },
+        }"#,
+    );
+
+    c.bench_function("validate_json model", |b| {
+        b.iter(|| {
+            let input_json = black_box(
+                r#"
+                {
+                    "a": 1,
+                    "b": 2,
+                    "c": 3,
+                    "d": 4,
+                    "e": 5
+                }
+                "#
+                .to_string(),
+            );
+            validator.validate_json(py, input_json).unwrap()
+        })
+    });
+}
+
+criterion_group! {
     name = benches;
     config = Criterion::default().warm_up_time(Duration::from_secs(1)).measurement_time(Duration::from_secs(3));
-    targets =  benchmark_ints, benchmark_dict
+    targets =  benchmark_ints, benchmark_dict, benchmark_model
 }
 criterion_main!(benches);
