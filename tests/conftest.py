e860c6f1bca1fee7d9e0e778c07c64a73e68910b
messense <messense@icloud.com>
2022-06-23 20:49:34 +0800

    Run Emscripten unit tests with Pyodide

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import importlib.util
 import json
 import os
+import sys
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
@@ -79,3 +80,13 @@ def import_execute(request, tmp_work_path: Path):
             print('KeyboardInterrupt')
 
     return _import_execute
+
+
+if sys.platform == 'emscripten':
+
+    @pytest.fixture
+    def benchmark():
+        def result(func, *args, **kwargs):
+            return func(*args, **kwargs)
+
+        return result
