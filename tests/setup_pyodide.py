08b38283aa3ec09ecb683dba5489d6eabf22fc66
Samuel Colvin <s@muelcolvin.com>
2022-07-05 22:14:28 +0100

    remove nox, fix

--- tests/setup_pyodide.py
+++ tests/setup_pyodide.py
@@ -9,13 +9,13 @@ from pathlib import Path
 
 def main():
     root_dir = Path(__file__).resolve().parent.parent
-    with os.chdir(root_dir / 'node_modules' / 'pyodide'):
-        subprocess.run(['node', '../prettier/bin-prettier.js', '-w', 'pyodide.asm.js'], check=True)
-        with open('repodata.json') as f:
-            emscripten_version = json.load(f)['info']['platform'].split('_', 1)[1].replace('_', '.')
-            if github_env := os.getenv('GITHUB_ENV'):
-                with open(github_env, 'w+') as f:
-                    f.write(f'EMSCRIPTEN_VERSION={emscripten_version}\n')
+    os.chdir(root_dir / 'node_modules' / 'pyodide')
+    subprocess.run(['node', '../prettier/bin-prettier.js', '-w', 'pyodide.asm.js'], check=True)
+    with open('repodata.json') as f:
+        emscripten_version = json.load(f)['info']['platform'].split('_', 1)[1].replace('_', '.')
+        if github_env := os.getenv('GITHUB_ENV'):
+            with open(github_env, 'w+') as github_env_file:
+                github_env_file.write(f'EMSCRIPTEN_VERSION={emscripten_version}\n')
 
 
 if __name__ == '__main__':
