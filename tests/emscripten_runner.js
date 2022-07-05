825a1c9786c1568aa388d30aad2728a771618e56
Samuel Colvin <s@muelcolvin.com>
2022-07-05 21:05:41 +0100

    experiment in simplifying

--- tests/emscripten_runner.js
+++ tests/emscripten_runner.js
@@ -1,20 +1,20 @@
-const { opendir } = require('node:fs/promises');
-const { loadPyodide } = require('pyodide');
+const {opendir} = require('node:fs/promises')
+const {loadPyodide} = require('pyodide')
 
 async function findWheel(distDir) {
-  const dir = await opendir(distDir);
+  const dir = await opendir(distDir)
   for await (const dirent of dir) {
     if (dirent.name.endsWith('whl')) {
-      return dirent.name;
+      return dirent.name
     }
   }
 }
 
-const pkgDir = process.argv[2];
-const distDir = pkgDir + '/dist';
-const testDir = pkgDir + '/tests';
+const pkgDir = process.cwd()
+const distDir = pkgDir + '/dist'
+const testDir = pkgDir + '/tests'
 
-function make_tty_ops(stream){
+function make_tty_ops(stream) {
   return {
     // get_char has 3 particular return values:
     // a.) the next character represented as an integer
@@ -22,88 +22,87 @@ function make_tty_ops(stream){
     // c.) null to signal an EOF
     get_char(tty) {
       if (!tty.input.length) {
-        var result = null;
-        var BUFSIZE = 256;
-        var buf = Buffer.alloc(BUFSIZE);
-        var bytesRead = fs.readSync(process.stdin.fd, buf, 0, BUFSIZE, -1);
+        var result = null
+        var BUFSIZE = 256
+        var buf = Buffer.alloc(BUFSIZE)
+        var bytesRead = fs.readSync(process.stdin.fd, buf, 0, BUFSIZE, -1)
         if (bytesRead === 0) {
-          return null;
+          return null
         }
-        result = buf.slice(0, bytesRead);
-        tty.input = Array.from(result);
+        result = buf.slice(0, bytesRead)
+        tty.input = Array.from(result)
       }
-      return tty.input.shift();
+      return tty.input.shift()
     },
     put_char(tty, val) {
       try {
-        if(val !== null){
-          tty.output.push(val);
+        if (val !== null) {
+          tty.output.push(val)
         }
         if (val === null || val === 10) {
-          process.stdout.write(Buffer.from(tty.output));
-          tty.output = [];
+          process.stdout.write(Buffer.from(tty.output))
+          tty.output = []
         }
-      } catch(e){
-        console.warn(e);
+      } catch (e) {
+        console.warn(e)
       }
     },
     flush(tty) {
       if (!tty.output || tty.output.length === 0) {
-        return;
+        return
       }
-      stream.write(Buffer.from(tty.output));
-      tty.output = [];
-    }
-  };
+      stream.write(Buffer.from(tty.output))
+      tty.output = []
+    },
+  }
 }
 
-function setupStreams(FS, TTY){
-  let mytty = FS.makedev(FS.createDevice.major++, 0);
-  let myttyerr = FS.makedev(FS.createDevice.major++, 0);
+function setupStreams(FS, TTY) {
+  let mytty = FS.makedev(FS.createDevice.major++, 0)
+  let myttyerr = FS.makedev(FS.createDevice.major++, 0)
   TTY.register(mytty, make_tty_ops(process.stdout))
   TTY.register(myttyerr, make_tty_ops(process.stderr))
-  FS.mkdev('/dev/mytty', mytty);
-  FS.mkdev('/dev/myttyerr', myttyerr);
-  FS.unlink('/dev/stdin');
-  FS.unlink('/dev/stdout');
-  FS.unlink('/dev/stderr');
-  FS.symlink('/dev/mytty', '/dev/stdin');
-  FS.symlink('/dev/mytty', '/dev/stdout');
-  FS.symlink('/dev/myttyerr', '/dev/stderr');
-  FS.closeStream(0);
-  FS.closeStream(1);
-  FS.closeStream(2);
-  var stdin = FS.open('/dev/stdin', 0);
-  var stdout = FS.open('/dev/stdout', 1);
-  var stderr = FS.open('/dev/stderr', 1);
+  FS.mkdev('/dev/mytty', mytty)
+  FS.mkdev('/dev/myttyerr', myttyerr)
+  FS.unlink('/dev/stdin')
+  FS.unlink('/dev/stdout')
+  FS.unlink('/dev/stderr')
+  FS.symlink('/dev/mytty', '/dev/stdin')
+  FS.symlink('/dev/mytty', '/dev/stdout')
+  FS.symlink('/dev/myttyerr', '/dev/stderr')
+  FS.closeStream(0)
+  FS.closeStream(1)
+  FS.closeStream(2)
+  FS.open('/dev/stdin', 0)
+  FS.open('/dev/stdout', 1)
+  FS.open('/dev/stderr', 1)
 }
-
 
 async function main() {
-  const wheelName = await findWheel(distDir);
-  const wheelURL = `file:${distDir}/${wheelName}`;
-  let errcode = 0;
+  const wheelName = await findWheel(distDir)
+  const wheelURL = `file:${distDir}/${wheelName}`
+  let errcode = 0
   try {
-    pyodide = await loadPyodide();
-    const FS = pyodide.FS;
-    setupStreams(FS, pyodide._module.TTY);
-    const NODEFS = FS.filesystems.NODEFS;
-    FS.mkdir('/test_dir');
-    FS.mount(NODEFS, { root: testDir }, '/test_dir');
-    await pyodide.loadPackage(['micropip', 'pytest', 'pytz']);
-    const micropip = pyodide.pyimport('micropip');
-    await micropip.install('dirty-equals');
-    await micropip.install('hypothesis');
-    await micropip.install('pytest-speed');
-    await micropip.install(wheelURL);
-    const pytest = pyodide.pyimport('pytest');
-    FS.chdir("/test_dir");
-    errcode = pytest.main();
+    const pyodide = await loadPyodide()
+    const FS = pyodide.FS
+    setupStreams(FS, pyodide._module.TTY)
+    const NODEFS = FS.filesystems.NODEFS
+    FS.mkdir('/test_dir')
+    FS.mount(NODEFS, {root: testDir}, '/test_dir')
+    await pyodide.loadPackage(['micropip', 'pytest', 'pytz'])
+    const micropip = pyodide.pyimport('micropip')
+    await micropip.install('dirty-equals')
+    await micropip.install('hypothesis')
+    await micropip.install('pytest-speed')
+    await micropip.install(wheelURL)
+    const pytest = pyodide.pyimport('pytest')
+    FS.chdir('/test_dir')
+    errcode = pytest.main()
   } catch (e) {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   }
-  process.exit(errcode);
+  process.exit(errcode)
 }
 
-main();
+main()
--- tests/noxfile.py
+++ tests/noxfile.py
@@ -7,34 +7,26 @@ from pathlib import Path
 
 import nox
 
-PYODIDE_VERSION = os.getenv('PYODIDE_VERSION', '0.21.0-alpha.2')
-GITHUB_ACTIONS = os.getenv('GITHUB_ACTIONS')
-GITHUB_ENV = os.getenv('GITHUB_ENV')
+PYODIDE_VERSION = '0.21.0-alpha.2'
 
 
 def append_to_github_env(name: str, value: str):
-    if not GITHUB_ACTIONS or not GITHUB_ENV:
-        return
-
-    with open(GITHUB_ENV, 'w+') as f:
-        f.write(f'{name}={value}\n')
+    if github_env := os.getenv('GITHUB_ENV'):
+        with open(github_env, 'w+') as f:
+            f.write(f'{name}={value}\n')
 
 
 @nox.session(name='setup-pyodide')
 def setup_pyodide(session: nox.Session):
-    tests_dir = Path(__file__).resolve().parent
-    with session.chdir(tests_dir):
-        session.run('npm', 'i', '--no-save', f'pyodide@{PYODIDE_VERSION}', 'prettier', external=True)
-        with session.chdir(tests_dir / 'node_modules' / 'pyodide'):
-            session.run('node', '../prettier/bin-prettier.js', '-w', 'pyodide.asm.js', external=True)
-            with open('repodata.json') as f:
-                emscripten_version = json.load(f)['info']['platform'].split('_', 1)[1].replace('_', '.')
-                append_to_github_env('EMSCRIPTEN_VERSION', emscripten_version)
+    root_dir = Path(__file__).resolve().parent.parent
+    with session.chdir(root_dir / 'node_modules' / 'pyodide'):
+        session.run('node', '../prettier/bin-prettier.js', '-w', 'pyodide.asm.js', external=True)
+        with open('repodata.json') as f:
+            emscripten_version = json.load(f)['info']['platform'].split('_', 1)[1].replace('_', '.')
+            append_to_github_env('EMSCRIPTEN_VERSION', emscripten_version)
 
 
 @nox.session(name='test-emscripten')
 def test_emscripten(session: nox.Session):
-    tests_dir = Path(__file__).resolve().parent
-    crate = tests_dir.parent
-    with session.chdir(tests_dir):
-        session.run('node', 'emscripten_runner.js', str(crate), external=True)
+    root_dir = Path(__file__).resolve().parent.parent
+    session.run('node', 'emscripten_runner.js', str(root_dir), external=True)
