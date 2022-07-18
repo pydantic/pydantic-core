fb7788112b9d4a5f71cff56d45a19d38e9ced8be
Samuel Colvin <s@muelcolvin.com>
2022-07-18 15:07:43 +0100

    try cargo:rerun-if-changed

--- build.rs
+++ build.rs
@@ -1,13 +1,10 @@
-use std::path::Path;
 use std::process::Command;
 use std::str::from_utf8;
 
 fn generate_self_schema() {
-    if Path::new("./src/self_schema.py").exists() && option_env!("DEBIAN_FRONTEND") == Some("noninteractive") {
-        // self_schema.py already exists and DEBIAN_FRONTEND indicates we're in a maturin build,
-        // skip running generate_self_schema.py
-        return;
-    }
+    println!("cargo:rerun-if-changed=pydantic_core/_types.py");
+    println!("cargo:rerun-if-changed=generate_self_schema.py");
+
     let output = Command::new("python3")
         .arg("generate_self_schema.py")
         .output()
