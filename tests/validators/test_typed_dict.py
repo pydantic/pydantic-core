16740baf8498c7d7fc982bbee44834ac408bff92
Sergey Tsaplin <me@sergeytsaplin.com>
2022-07-19 17:39:59 +0200

    Validate both json and python

--- tests/validators/test_typed_dict.py
+++ tests/validators/test_typed_dict.py
@@ -460,20 +460,17 @@ def test_alias(py_and_json: PyAndJson):
         assert v.validate_test({'field_a': '123'})
 
 
-def test_empty_string_field_name():
-    v = SchemaValidator({'type': 'typed-dict', 'fields': {'': {'schema': {'type': 'int'}}}})
-    input_str = '{"": 123}'
-    assert v.validate_json(input_str) == {'': 123}
+def test_empty_string_field_name(py_and_json: PyAndJson):
+    v = py_and_json({'type': 'typed-dict', 'fields': {'': {'schema': {'type': 'int'}}}})
+    assert v.validate_test({'': 123}) == {'': 123}
 
 
-def test_empty_string_aliases():
-    v = SchemaValidator({'type': 'typed-dict', 'fields': {'field_a': {'alias': '', 'schema': {'type': 'int'}}}})
-    input_str = '{"": 123}'
-    assert v.validate_json(input_str) == {'field_a': 123}
+def test_empty_string_aliases(py_and_json: PyAndJson):
+    v = py_and_json({'type': 'typed-dict', 'fields': {'field_a': {'alias': '', 'schema': {'type': 'int'}}}})
+    assert v.validate_test({'': 123}) == {'field_a': 123}
 
-    v = SchemaValidator({'type': 'typed-dict', 'fields': {'field_a': {'alias': ['', ''], 'schema': {'type': 'int'}}}})
-    input_str = '{"": {"": 123}}'
-    assert v.validate_json(input_str) == {'field_a': 123}
+    v = py_and_json({'type': 'typed-dict', 'fields': {'field_a': {'alias': ['', ''], 'schema': {'type': 'int'}}}})
+    assert v.validate_test({'': {'': 123}}) == {'field_a': 123}
 
 
 def test_alias_allow_pop(py_and_json: PyAndJson):
