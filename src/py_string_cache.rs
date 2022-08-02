baa57d7f08837977aa0edfd0c379fb1689bec027
Samuel Colvin <s@muelcolvin.com>
2022-08-02 16:47:10 +0100

    experiment with custom hasher

--- src/py_string_cache.rs
+++ src/py_string_cache.rs
@@ -1,11 +1,15 @@
+use std::collections::HashMap;
+use std::hash::{BuildHasherDefault, Hasher};
+use std::sync::{Arc, Mutex};
+
 use pyo3::once_cell::GILOnceCell;
 use pyo3::prelude::*;
 use pyo3::types::PyString;
-use std::sync::{Arc, Mutex};
 
-use ahash::AHashMap;
+use ahash::AHasher;
+use nohash_hasher::BuildNoHashHasher;
 
-type PyStringCache = Arc<Mutex<AHashMap<String, Py<PyString>>>>;
+type PyStringCache = Arc<Mutex<HashMap<u64, Py<PyString>, BuildNoHashHasher<u64>>>>;
 static PY_STRING_CACHE: GILOnceCell<PyStringCache> = GILOnceCell::new();
 
 const LENGTH_LIMIT: usize = 63;
@@ -17,10 +21,19 @@ pub fn make_py_string<'py>(py: Python<'py>, s: &str) -> &'py PyString {
         return PyString::new(py, s);
     }
 
-    let cache = PY_STRING_CACHE.get_or_init(py, || Arc::new(Mutex::new(AHashMap::with_capacity(100))));
+    let cache = PY_STRING_CACHE.get_or_init(py, || {
+        Arc::new(Mutex::new(HashMap::with_capacity_and_hasher(
+            10,
+            BuildHasherDefault::default(),
+        )))
+    });
+
+    let mut hasher = AHasher::new_with_keys(1, 1);
+    hasher.write(s.as_bytes());
+    let key = hasher.finish();
 
     let mut hashmap = cache.lock().expect("Failed to acquire PY_STRING_CACHE lock");
-    if let Some(py_string) = hashmap.get(s) {
+    if let Some(py_string) = hashmap.get(&key) {
         py_string.clone_ref(py).into_ref(py)
     } else {
         if hashmap.len() >= MAX_ITEMS {
@@ -30,7 +43,7 @@ pub fn make_py_string<'py>(py: Python<'py>, s: &str) -> &'py PyString {
             }
         }
         let py_string = PyString::new(py, s);
-        hashmap.insert(s.to_string(), py_string.into_py(py));
+        hashmap.insert(key, py_string.into_py(py));
         py_string
     }
 }
