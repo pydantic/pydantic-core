29b1762fd3c736be68f2467a4e8f95df378570b2
Samuel Colvin <s@muelcolvin.com>
2022-08-03 06:56:06 +0100

    use associative cache

--- src/py_string_cache.rs
+++ src/py_string_cache.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-use std::hash::{BuildHasherDefault, Hasher};
+use std::hash::Hasher;
 use std::sync::{Arc, Mutex};
 
 use pyo3::once_cell::GILOnceCell;
@@ -7,18 +6,16 @@ use pyo3::prelude::*;
 use pyo3::types::PyString;
 
 use ahash::AHasher;
-use nohash_hasher::BuildNoHashHasher;
+use associative_cache::*;
 
 type PyStringCache = (
     u128,
     u128,
-    Arc<Mutex<HashMap<u64, Py<PyString>, BuildNoHashHasher<u64>>>>,
+    Arc<Mutex<AssociativeCache<u64, Py<PyString>, Capacity8192, HashDirectMapped, RoundRobinReplacement>>>,
 );
 static PY_STRING_CACHE: GILOnceCell<PyStringCache> = GILOnceCell::new();
 
 const LENGTH_LIMIT: usize = 63;
-// gives max cache size of ~2MB
-const MAX_ITEMS: usize = 32_000;
 
 pub fn make_py_string<'py>(py: Python<'py>, s: &str) -> &'py PyString {
     if s.len() > LENGTH_LIMIT {
@@ -26,26 +23,17 @@ pub fn make_py_string<'py>(py: Python<'py>, s: &str) -> &'py PyString {
     }
 
     let (key1, key2, cache) = PY_STRING_CACHE.get_or_init(py, || {
-        let hashmap = HashMap::with_capacity_and_hasher(100, BuildHasherDefault::default());
-        (123, 321, Arc::new(Mutex::new(hashmap)))
+        let cache = AssociativeCache::default();
+        (123, 321, Arc::new(Mutex::new(cache)))
     });
 
     let mut hasher = AHasher::new_with_keys(*key1, *key2);
     hasher.write(s.as_bytes());
-    let key = hasher.finish();
+    let hash = hasher.finish();
 
-    let mut hashmap = cache.lock().expect("Failed to acquire PY_STRING_CACHE lock");
-    if let Some(py_string) = hashmap.get(&key) {
-        py_string.clone_ref(py).into_ref(py)
-    } else {
-        if hashmap.len() >= MAX_ITEMS {
-            let keys: Vec<_> = hashmap.keys().take(1000).cloned().collect();
-            for k in keys {
-                hashmap.remove(&k);
-            }
-        }
-        let py_string = PyString::new(py, s);
-        hashmap.insert(key, py_string.into_py(py));
-        py_string
-    }
+    let mut cache = cache.lock().expect("Failed to acquire PY_STRING_CACHE lock");
+    let py_string = cache
+        .entry(&hash)
+        .or_insert_with(|| hash, || PyString::new(py, s).into_py(py));
+    py_string.clone_ref(py).into_ref(py)
 }
