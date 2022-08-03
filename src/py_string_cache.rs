e814dccc4c6af530cb712bbf808c04ec6d46d915
Samuel Colvin <s@muelcolvin.com>
2022-08-03 10:47:30 +0100

    use random state

--- src/py_string_cache.rs
+++ src/py_string_cache.rs
@@ -1,43 +1,39 @@
 use std::collections::HashMap;
-use std::hash::{BuildHasherDefault, Hasher};
+use std::hash::{BuildHasher, BuildHasherDefault, Hasher};
 use std::sync::{Arc, Mutex};
 
 use pyo3::once_cell::GILOnceCell;
 use pyo3::prelude::*;
 use pyo3::types::PyString;
 
-use ahash::AHasher;
+use ahash::RandomState;
 use nohash_hasher::BuildNoHashHasher;
 
-type PyStringCache = Arc<Mutex<HashMap<u64, Py<PyString>, BuildNoHashHasher<u64>>>>;
+type PyStringCache = Arc<Mutex<(RandomState, HashMap<u64, Py<PyString>, BuildNoHashHasher<u64>>)>>;
 static PY_STRING_CACHE: GILOnceCell<PyStringCache> = GILOnceCell::new();
 
 const LENGTH_LIMIT: usize = 63;
-// gives max cache size of ~2MB
-const MAX_ITEMS: usize = 32_000;
+// gives max cache size of ~1MB
+const MAX_ITEMS: usize = 16_000;
 
 pub fn make_py_string<'py>(py: Python<'py>, s: &str) -> &'py PyString {
     if s.len() > LENGTH_LIMIT {
         return PyString::new(py, s);
     }
 
-    let cache = PY_STRING_CACHE.get_or_init(py, || {
-        Arc::new(Mutex::new(HashMap::with_capacity_and_hasher(
-            10,
-            BuildHasherDefault::default(),
-        )))
-    });
+    let cache = PY_STRING_CACHE.get_or_init(py, create_cache);
 
-    let mut hasher = AHasher::new_with_keys(1, 1);
+    let (random_state, hashmap) = &mut *cache.lock().expect("Failed to acquire PY_STRING_CACHE lock");
+
+    let mut hasher = random_state.build_hasher();
     hasher.write(s.as_bytes());
     let key = hasher.finish();
 
-    let mut hashmap = cache.lock().expect("Failed to acquire PY_STRING_CACHE lock");
     if let Some(py_string) = hashmap.get(&key) {
         py_string.clone_ref(py).into_ref(py)
     } else {
         if hashmap.len() >= MAX_ITEMS {
-            let keys: Vec<_> = hashmap.keys().take(1000).cloned().collect();
+            let keys: Vec<_> = hashmap.keys().take(200).cloned().collect();
             for k in keys {
                 hashmap.remove(&k);
             }
@@ -46,4 +42,10 @@ pub fn make_py_string<'py>(py: Python<'py>, s: &str) -> &'py PyString {
         hashmap.insert(key, py_string.into_py(py));
         py_string
     }
+}
+
+fn create_cache() -> PyStringCache {
+    let random_state = RandomState::new();
+    let cache = HashMap::with_capacity_and_hasher(10, BuildHasherDefault::default());
+    Arc::new(Mutex::new((random_state, cache)))
 }
