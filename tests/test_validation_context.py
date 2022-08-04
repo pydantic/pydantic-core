a9319b4221d3a4cd8200018a2c25f09ce4919370
Samuel Colvin <s@muelcolvin.com>
2022-08-04 15:07:37 +0100

    fix tests and use py_and_json

--- tests/test_validation_context.py
+++ tests/test_validation_context.py
@@ -1,32 +1,33 @@
 import pytest
 
-from pydantic_core import SchemaValidator, ValidationError
+from pydantic_core import ValidationError
+
+from .conftest import PyAndJson
 
 
-def test_after():
+def test_after(py_and_json: PyAndJson):
     def f(input_value, *, context, **kwargs):
         return input_value + f'| context: {context}'
 
-    v = SchemaValidator({'type': 'function', 'mode': 'after', 'function': f, 'schema': 'str'})
+    v = py_and_json({'type': 'function', 'mode': 'after', 'function': f, 'schema': 'str'})
 
-    assert v.validate_python('foobar') == 'foobar| context: None'
-    assert v.validate_python('foobar', None, {1: 10}) == 'foobar| context: {1: 10}'
-    assert v.validate_json('"foobar"', None, {1: 10}) == 'foobar| context: {1: 10}'
-    assert v.validate_python('foobar', None, 'frogspawn') == 'foobar| context: frogspawn'
+    assert v.validate_test('foobar') == 'foobar| context: None'
+    assert v.validate_test('foobar', None, {1: 10}) == 'foobar| context: {1: 10}'
+    assert v.validate_test('foobar', None, 'frogspawn') == 'foobar| context: frogspawn'
 
 
-def test_mutable_context():
+def test_mutable_context(py_and_json: PyAndJson):
     def f(input_value, *, context, **kwargs):
         context['foo'] = input_value
         return input_value
 
-    v = SchemaValidator({'type': 'function', 'mode': 'before', 'function': f, 'schema': 'str'})
+    v = py_and_json({'type': 'function', 'mode': 'before', 'function': f, 'schema': 'str'})
     mutable_context = {}
-    assert v.validate_python('foobar', None, mutable_context) == 'foobar'
+    assert v.validate_test('foobar', None, mutable_context) == 'foobar'
     assert mutable_context == {'foo': 'foobar'}
 
 
-def test_typed_dict():
+def test_typed_dict(py_and_json: PyAndJson):
     def f1(input_value, *, context, **kwargs):
         context['f1'] = input_value
         return input_value + f'| context: {context}'
@@ -35,7 +36,7 @@ def test_typed_dict():
         context['f2'] = input_value
         return input_value + f'| context: {context}'
 
-    v = SchemaValidator(
+    v = py_and_json(
         {
             'type': 'typed-dict',
             'fields': {
@@ -45,52 +46,46 @@ def test_typed_dict():
         }
     )
 
-    assert v.validate_python({'f1': '1', 'f2': '2'}, None, {'x': 'y'}) == {
+    assert v.validate_test({'f1': '1', 'f2': '2'}, None, {'x': 'y'}) == {
         'f1': "1| context: {'x': 'y', 'f1': '1'}",
         'f2': "2| context: {'x': 'y', 'f1': '1', 'f2': '2'}",
     }
 
 
-def test_wrap():
+def test_wrap(py_and_json: PyAndJson):
     def f(input_value, *, validator, context, **kwargs):
         return validator(input_value) + f'| context: {context}'
 
-    v = SchemaValidator({'type': 'function', 'mode': 'wrap', 'function': f, 'schema': 'str'})
+    v = py_and_json({'type': 'function', 'mode': 'wrap', 'function': f, 'schema': 'str'})
 
-    assert v.validate_python('foobar') == 'foobar| context: None'
-    assert v.validate_python('foobar', None, {1: 10}) == 'foobar| context: {1: 10}'
-    assert v.validate_json('"foobar"', None, {1: 10}) == 'foobar| context: {1: 10}'
-    assert v.validate_python('foobar', None, 'frogspawn') == 'foobar| context: frogspawn'
+    assert v.validate_test('foobar') == 'foobar| context: None'
+    assert v.validate_test('foobar', None, {1: 10}) == 'foobar| context: {1: 10}'
+    assert v.validate_test('foobar', None, 'frogspawn') == 'foobar| context: frogspawn'
 
 
-def test_isinstance():
+def test_isinstance(py_and_json: PyAndJson):
     def f(input_value, *, validator, context, **kwargs):
         if 'error' in context:
             raise ValueError('wrong')
         return validator(input_value)
 
-    v = SchemaValidator({'type': 'function', 'mode': 'wrap', 'function': f, 'schema': 'str'})
+    v = py_and_json({'type': 'function', 'mode': 'wrap', 'function': f, 'schema': 'str'})
 
     assert v.validate_python('foobar', None, {}) == 'foobar'
-    assert v.validate_json('"foobar"', None, {}) == 'foobar'
 
-    with pytest.raises(TypeError, match="argument of type 'NoneType' is not iterable"):  # internal error!
-        v.validate_python('foobar')
+    # internal error!, use generic bit of error message to match both cpython and pypy
+    with pytest.raises(TypeError, match='is not iterable'):
+        v.validate_test('foobar')
 
-    with pytest.raises(TypeError, match="argument of type 'NoneType' is not iterable"):  # internal error!
-        v.isinstance_python('foobar')
-
-    with pytest.raises(ValidationError, match=r'Value error, wrong \[kind=value_error,'):
-        v.validate_python('foobar', None, {'error'})
+    with pytest.raises(TypeError, match='is not iterable'):
+        v.isinstance_test('foobar')
 
     with pytest.raises(ValidationError, match=r'Value error, wrong \[kind=value_error,'):
-        v.validate_json('"foobar"', None, {'error'})
+        v.validate_test('foobar', None, {'error'})
 
-    assert v.isinstance_python('foobar', None, {}) is True
-    assert v.isinstance_json('"foobar"', None, {}) is True
+    assert v.isinstance_test('foobar', None, {}) is True
 
-    with pytest.raises(TypeError, match="argument of type 'NoneType' is not iterable"):  # internal error!
-        v.isinstance_python('foobar')
+    with pytest.raises(TypeError, match='is not iterable'):
+        v.isinstance_test('foobar')
 
-    assert v.isinstance_python('foobar', None, {'error'}) is False
-    assert v.isinstance_json('"foobar"', None, {'error'}) is False
+    assert v.isinstance_test('foobar', None, {'error'}) is False
