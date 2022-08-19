866a9df452ba4165c6252b2df4f8b408d68cd7e3
Samuel Colvin <s@muelcolvin.com>
2022-08-19 13:26:13 +0100

    cleanup coverage export

--- tests/rust_coverage_export.sh
+++ tests/rust_coverage_export.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 rust-profdata merge -sparse *.profraw -o default.profdata
 
-rust-cov report -Xdemangler=rustfilt $(ls pydantic_core/*.so) \
+rust-cov report -Xdemangler=rustfilt pydantic_core/*.so \
     -instr-profile=default.profdata \
-    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='\.cargo/registry' \
     --ignore-filename-regex='library/std' \
 
-rust-cov export -Xdemangler=rustfilt $(ls pydantic_core/*.so) \
+rust-cov export -Xdemangler=rustfilt pydantic_core/*.so \
     -instr-profile=default.profdata \
-    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='\.cargo/registry' \
     --ignore-filename-regex='library/std' \
     -format=lcov > default.lcov
 
--- tests/rust_coverage_html.sh
+++ tests/rust_coverage_html.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-rust-profdata merge -sparse *.profraw -o default.profdata
+rust-profdata merge -sparse *.profraw -o rust_coverage.profdata
 
-rust-cov report -Xdemangler=rustfilt $(ls pydantic_core/*.so) \
-    -instr-profile=default.profdata \
-    --ignore-filename-regex='/.cargo/registry' \
+rust-cov report -Xdemangler=rustfilt pydantic_core/*.so \
+    -instr-profile=rust_coverage.profdata \
+    --ignore-filename-regex='\.cargo/registry' \
     --ignore-filename-regex='library/std' \
 
-rust-cov show -Xdemangler=rustfilt $(ls pydantic_core/*.so) \
-    -instr-profile=default.profdata \
-    --ignore-filename-regex='/.cargo/registry' \
+rust-cov show -Xdemangler=rustfilt pydantic_core/*.so \
+    -instr-profile=rust_coverage.profdata \
+    --ignore-filename-regex='\.cargo/registry' \
     --ignore-filename-regex='library/std' \
     -format=html -o htmlcov/rust
 
-rm -f *.profraw default.profdata
+rm -f *.profraw rust_coverage.profdata
