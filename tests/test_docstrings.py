1ed9f3caaf95f1d03617707e2f29d658e3981f0a
Phil Chen <philchen2000@gmail.com>
2022-12-27 16:35:40 -0800

    improve test

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -32,16 +32,29 @@ class DocstringTest:
         pass
 
 
+class DocstringTestBadIndent:
+    def method_a(self):
+        """
+          ```python
+          assert 1 == 1
+        print('badly indented line')
+          ```
+        """
+        pass
+
+
 def write_docstrings_to_test_file(obj_with_docstrings: Any, f: TextIO):
     for name, obj in inspect.getmembers(obj_with_docstrings):
         if obj.__doc__ is not None:
             for i, match in enumerate(re.finditer(DOCSTRING_REGEX, obj.__doc__, re.DOTALL)):
                 code = match.group(1)
                 f.write(f'def test_{name}_{i}():\n')
-                for line in code.splitlines():
-                    if line.strip():
-                        f.write(line)
-                        f.write('\n')
+                lines = [line for line in code.splitlines() if line.strip()]
+                initial_indent = len(lines[0]) - len(lines[0].lstrip())
+                for line in lines:
+                    if line[:initial_indent].strip():
+                        raise ValueError(f'Unexpected indentation: {line}')
+                    f.write(f'    {line[initial_indent:]}\n')
                 f.write('\n')
     f.flush()
 
@@ -52,15 +65,20 @@ def test_write_docstrings_to_test_file():
         assert (
             f.getvalue()
             == """def test_method_a_0():
-        assert 1 == 1
-        assert 1 != 2
+    assert 1 == 1
+    assert 1 != 2
 
 def test_method_b_0():
-        print('hello')
-        print('world')
+    print('hello')
+    print('world')
 
 """
         )
+
+
+def test_write_docstrings_to_test_file_raises_value_error():
+    with StringIO('') as f, pytest.raises(ValueError):
+        write_docstrings_to_test_file(DocstringTestBadIndent, f)
 
 
 def test_docstrings():
