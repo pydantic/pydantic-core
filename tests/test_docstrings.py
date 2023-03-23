b7b5bd7a745dadfab4393d1c4f7c60550f250859
Samuel Colvin <s@muelcolvin.com>
2023-03-23 21:52:30 +0000

    all examples formatted

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -13,24 +13,24 @@ except ImportError:
 
 
 @pytest.mark.skipif(sys.platform not in {'linux', 'darwin'}, reason='Only both on linux and macos')
-@pytest.mark.parametrize('example', find_examples('pydantic_core/core_schema.py'))
+@pytest.mark.parametrize('example', find_examples('pydantic_core/core_schema.py'), ids=str)
 def test_docstrings(example: CodeExample, eval_example: EvalExample):
     eval_example.set_config(quotes='single')
 
     if eval_example.update_examples:
         eval_example.format(example)
-        eval_example.run_print_update(example, rewrite_assertions=True)
+        eval_example.run_print_update(example)
     else:
         eval_example.lint(example)
-        eval_example.run_print_check(example, rewrite_assertions=True)
+        eval_example.run_print_check(example)
 
 
 @pytest.mark.skipif(sys.platform not in {'linux', 'darwin'}, reason='Only both on linux and macos')
-@pytest.mark.parametrize('example', find_examples('README.md'))
+@pytest.mark.parametrize('example', find_examples('README.md'), ids=str)
 def test_readme(example: CodeExample, eval_example: EvalExample):
     eval_example.set_config(line_length=100, quotes='single')
     if eval_example.update_examples:
         eval_example.format(example)
     else:
         eval_example.lint(example)
-        eval_example.run(example, rewrite_assertions=True)
+        eval_example.run(example)
