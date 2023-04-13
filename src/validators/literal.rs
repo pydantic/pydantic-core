38e5d219fda822ca8d91eae0e2386d6b07cd6fee
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2023-04-13 13:47:52 -0500

    Properly differentiate between ints and IntEnum

--- src/validators/literal.rs
+++ src/validators/literal.rs
@@ -1,3 +1,4 @@
+use ahash::{HashMap, HashMapExt};
 use pyo3::intern;
 use pyo3::prelude::*;
 use pyo3::types::{PyDict, PyList};
@@ -11,9 +12,25 @@ use super::{BuildContext, BuildValidator, CombinedValidator, Extra, Validator};
 
 #[derive(Debug, Clone)]
 pub struct LiteralValidator {
-    expected_py: Py<PyDict>,
+    expected_int: Option<HashMap<i64, Py<PyAny>>>,
+    expected_str: Option<HashMap<String, Py<PyAny>>>,
+    expected_py: Option<Py<PyDict>>,
     expected_repr: String,
     name: String,
+}
+
+fn extract_int_strict(py: Python, item: &PyAny) -> Option<i64> {
+    if item.get_type().is(1.to_object(py).as_ref(py).get_type()) {
+        return Some(item.extract().unwrap());
+    }
+    None
+}
+
+fn extract_str_strict(py: Python, item: &PyAny) -> Option<String> {
+    if item.get_type().is(intern!(py, "a").get_type()) {
+        return Some(item.extract().unwrap());
+    }
+    None
 }
 
 impl BuildValidator for LiteralValidator {
@@ -30,15 +47,25 @@ impl BuildValidator for LiteralValidator {
         }
         let py = expected.py();
         // Literal[...] only supports int, str, bytes or enums, all of which can be hashed
+        let mut expected_int = HashMap::new();
+        let mut expected_str = HashMap::new();
         let expected_py = PyDict::new(py);
         let mut repr_args: Vec<String> = Vec::new();
         for item in expected.iter() {
             repr_args.push(item.repr()?.extract()?);
-            expected_py.set_item(item, item)?;
+            if let Some(int) = extract_int_strict(py, item) {
+                expected_int.insert(int, item.into());
+            } else if let Some(string) = extract_str_strict(py, item) {
+                expected_str.insert(string.to_string(), item.into());
+            } else {
+                expected_py.set_item(item, item)?;
+            }
         }
         let (expected_repr, name) = expected_repr_name(repr_args, "literal");
         Ok(CombinedValidator::Literal(Self {
-            expected_py: expected_py.into(),
+            expected_int: (!expected_int.is_empty()).then_some(expected_int),
+            expected_str: (!expected_str.is_empty()).then_some(expected_str),
+            expected_py: (!expected_py.is_empty()).then_some(expected_py.into()),
             expected_repr,
             name,
         }))
@@ -54,9 +81,25 @@ impl Validator for LiteralValidator {
         _slots: &'data [CombinedValidator],
         _recursion_guard: &'s mut RecursionGuard,
     ) -> ValResult<'data, PyObject> {
-        if let Some(v) = self.expected_py.as_ref(py).get_item(input) {
-            return Ok(v.into());
+        if let Some(expected_ints) = &self.expected_int {
+            if let Some(int) = extract_int_strict(py, input.to_object(py).as_ref(py)) {
+                if let Some(value) = expected_ints.get(&int) {
+                    return Ok(value.clone());
+                }
+            }
         }
+        if let Some(expected_strings) = &self.expected_str {
+            if let Some(string) = extract_str_strict(py, input.to_object(py).as_ref(py)) {
+                if let Some(value) = expected_strings.get(&string) {
+                    return Ok(value.clone());
+                }
+            }
+        }
+        if let Some(expected_py) = &self.expected_py {
+            if let Some(v) = expected_py.as_ref(py).get_item(input) {
+                return Ok(v.into());
+            }
+        };
         Err(ValError::new(
             ErrorType::LiteralError {
                 expected: self.expected_repr.clone(),
