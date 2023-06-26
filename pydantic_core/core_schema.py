7154222cc89910808046dd20e5879037f6f7e013
Fynn Beuttenmüller <thefynnbe@gmail.com>
2023-06-26 22:11:19 +0200

    fix context may be None
    The context kwarg of .model_validate defaults to None

--- pydantic_core/core_schema.py
+++ pydantic_core/core_schema.py
@@ -144,7 +144,7 @@ class ValidationInfo(Protocol):
     """
 
     @property
-    def context(self) -> Dict[str, Any]:
+    def context(self) -> Dict[str, Any] | None:
         """Current validation context."""
         ...
 
