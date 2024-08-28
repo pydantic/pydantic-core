b697ae3d792cf9dcab92f6d931136675623e942c
sydney-runkle <sydneymarierunkle@gmail.com>
2024-08-28 10:35:45 -0400

    add final example

--- python/pydantic_core/_pydantic_core.pyi
+++ python/pydantic_core/_pydantic_core.pyi
@@ -950,6 +950,32 @@ class PydanticUseDefault(Exception):
 
     This warning can be raised in custom valiation functions to redirect the flow of validation.
 
+    Example:
+        ```py
+        from pydantic_core import PydanticUseDefault
+        from datetime import datetime
+        from pydantic import BaseModel, field_validator
+
+
+        class Event(BaseModel):
+            name: str = 'meeting'
+            time: datetime
+
+            @field_validator('name', mode='plain')
+            def name_must_be_present(cls, v) -> str:
+                if not v or not isinstance(v, str):
+                    raise PydanticUseDefault()
+                return v
+
+
+        event1 = Event(name='party', time=datetime(2024, 1, 1, 12, 0, 0))
+        print(repr(event1))
+        # > Event(name='party', time=datetime.datetime(2024, 1, 1, 12, 0))
+        event2 = Event(time=datetime(2024, 1, 1, 12, 0, 0))
+        print(repr(event2))
+        # > Event(name='meeting', time=datetime.datetime(2024, 1, 1, 12, 0))
+        ```
+
     For an additional example, seethe [validating partial json data](../concepts/json.md#partial-json-parsing) section of the Pydantic documentation.
     """
 
