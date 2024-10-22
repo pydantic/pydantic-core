bd4689b948049475b9c77026fffbcca877971b8f
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-21 17:42:56 -0700

    commiting fix - handle errors in equality comparison in exclude defaults

--- src/serializers/fields.rs
+++ src/serializers/fields.rs
@@ -75,8 +75,10 @@ impl SerField {
 fn exclude_default(value: &Bound<'_, PyAny>, extra: &Extra, serializer: &CombinedSerializer) -> PyResult<bool> {
     if extra.exclude_defaults {
         if let Some(default) = serializer.get_default(value.py())? {
-            if value.eq(default)? {
-                return Ok(true);
+            return match value.eq(default) {
+                Ok(true) => Ok(true),
+                Ok(false) => Ok(false),
+                Err(_E) => Ok(false),
             }
         }
     }
