26df7ba142c0a7e27d20665b176131463f9c63fd
Huan-Cheng Chang <changhc84@gmail.com>
2024-12-11 22:42:24 +0000

    fix tests

--- tests/serializers/test_never.py
+++ tests/serializers/test_never.py
@@ -7,11 +7,11 @@ def test_to_python_never():
     v = SchemaSerializer(core_schema.never_schema())
     with pytest.raises(TypeError) as exc_info:
         v.to_python(1)
-    assert str(exc_info.value) == 'type `never` cannot be serialized'
+    assert str(exc_info.value) == 'Type `typing.Never` cannot be serialized'
 
 
 def test_to_json_never():
     v = SchemaSerializer(core_schema.never_schema())
     with pytest.raises(PydanticSerializationError) as exc_info:
         v.to_json('null')
-    assert 'type `never` cannot be serialized' in str(exc_info.value)
+    assert 'Type `typing.Never` cannot be serialized' in str(exc_info.value)
