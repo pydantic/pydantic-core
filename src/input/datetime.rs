86748f7b7a419245f44d939faa9b2bfa1ac2d0fe
Oliver Parker <oliver.parker@gridedge.co.uk>
2025-06-16 15:00:17 +0100

    style: formatting

--- src/input/datetime.rs
+++ src/input/datetime.rs
@@ -6,8 +6,8 @@ use pyo3::pyclass::CompareOp;
 use pyo3::types::PyTuple;
 use pyo3::types::{PyDate, PyDateTime, PyDelta, PyDeltaAccess, PyDict, PyTime, PyTzInfo};
 use pyo3::IntoPyObjectExt;
-use speedate::{DateTimeConfig, MicrosecondsPrecisionOverflowBehavior};
 use speedate::{Date, DateTime, Duration, ParseError, Time, TimeConfig};
+use speedate::{DateTimeConfig, MicrosecondsPrecisionOverflowBehavior};
 use std::borrow::Cow;
 use std::collections::hash_map::DefaultHasher;
 use std::fmt::Write;
@@ -366,12 +366,12 @@ pub fn bytes_as_datetime<'py>(
 ) -> ValResult<EitherDateTime<'py>> {
     match DateTime::parse_bytes_with_config(
         bytes,
-        &DateTimeConfig::builder().time_config(
-            TimeConfig {
+        &DateTimeConfig::builder()
+            .time_config(TimeConfig {
                 microseconds_precision_overflow_behavior: microseconds_overflow_behavior,
                 unix_timestamp_offset: Some(0),
-            },
-        ).build()
+            })
+            .build(),
     ) {
         Ok(dt) => Ok(dt.into()),
         Err(err) => Err(ValError::new(
@@ -392,12 +392,12 @@ pub fn int_as_datetime<'py>(
     match DateTime::from_timestamp_with_config(
         timestamp,
         timestamp_microseconds,
-        &DateTimeConfig::builder().time_config(
-            TimeConfig {
+        &DateTimeConfig::builder()
+            .time_config(TimeConfig {
                 unix_timestamp_offset: Some(0),
                 ..Default::default()
-            },
-        ).build()
+            })
+            .build(),
     ) {
         Ok(dt) => Ok(dt.into()),
         Err(err) => Err(ValError::new(
